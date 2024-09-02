95d817b4ad031874d55b65e09f0eaf4e7ae12eaf
Sahil <92850209+sahil26k@users.noreply.github.com>
2024-09-02 23:43:31 +0530

    Update Social.jsx

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -5,7 +5,7 @@ const Social = () =>{
          <a href=" https://github.com/sahil26k " className="home__social-icon" target="_blank">
          <i class="uil uil-github"></i>
          </a>
-         <a href=" https://https://www.linkedin.com/in/sahil-kashyap-0809a0135/" className="home__social-icon" target="_blank">
+         <a href=" https://www.linkedin.com/in/sahil-kashyap-0809a0135/" className="home__social-icon" target="_blank">
          <i class="uil uil-linkedin"></i>
          </a>
          <a href=" https://www.instagram.com/saa.hiii.ll/" className="home__social-icon" target="_blank">
